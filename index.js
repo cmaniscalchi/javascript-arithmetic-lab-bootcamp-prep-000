5e6845e4d18c3619370916e4e612a0842b4b0c26
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-05-09 19:52:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-
-var a = 1
-var b = 4
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
 
 function add() {
   return a + b
